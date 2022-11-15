70474afc721f5012ab1bd2f2c33ccfba8095c407
hossam-hub <hossam77ibrahim@gmail.com>
2022-11-15 14:07:42 +0300

    2:7

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
-import 'package:flutter/material.dart';
+//import 'package:fluent_ui/fluent_ui.dart';
 
-void main() {
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:sai_app/modules/login/login1.dart';
+import 'package:get/get.dart';
+import 'package:desktop_window/desktop_window.dart';
+
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows) {
+    await DesktopWindow.setMinWindowSize(Size(500, 850));
+    await DesktopWindow.setWindowSize(Size(300,340));
+  }
+
   runApp(const MyApp());
 }
 
@@ -10,7 +26,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      initialRoute: "/login",
+      getPages: [
+        GetPage(name: "/login", page: () => LoginScreen()),
+        //GetPage(name: "/login", page: () => MyCustomWidget()),
+      ],
+    );
   }
 }
 
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.green,
+//
+//       ),
+//     );
+//   }
+// }
